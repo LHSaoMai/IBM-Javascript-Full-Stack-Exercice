e4a4ecf333070f928a9e5f3ecf56be7611b7673f
LHSaoMai <lhoangsaomai@gmail.com>
2025-08-15 16:35:36 -0400

    exercice java grocery amount

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,3 +8,17 @@ function calculateArea() {
     let area = length*width;
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
    }
+
+let grocery1;
+let grocery2;
+let grocery3;
+
+function add(){
+    grocery1=parseFloat(document.getElementById('groc1').value);
+    grocery2=parseFloat(document.getElementById('groc2').value);
+    grocery3=parseFloat(document.getElementById('groc3').value);
+
+    let sumation= grocery1+grocery2+grocery3;
+
+    document.getElementById("groc_result").innerText = `The area amount is: $${sumation}`;
+}
