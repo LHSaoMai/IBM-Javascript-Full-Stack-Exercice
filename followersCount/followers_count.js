e92a24b903fcabfa05a96a5434da3afd9935c911
LHSaoMai <lhoangsaomai@gmail.com>
2025-08-16 05:25:17 -0400

    reset button follower

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -17,3 +17,13 @@ function checkCountValue() {
           alert("Your Instagram post gained 20 followers! Keep it up!");
         }
       }
+
+function reset(){
+    count = 0;
+    displayCount();
+    reset_message();
+}
+
+function reset_message(){
+    alert("Your account has been reset")
+}
