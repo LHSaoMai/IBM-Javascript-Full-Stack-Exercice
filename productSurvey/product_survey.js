b9f8a503f7a3152f52f2f0aefe126df9caebcf45
LHSaoMai <lhoangsaomai@gmail.com>
2025-08-16 12:34:58 -0400

    add exercice on product survey

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -11,6 +11,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experience = document.getElementById('experience').value;
 
     alert('Thank you for your valuable feedback');
     document.getElementById('userInfo').style.display = 'block';
@@ -21,6 +22,8 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = experience;
+
 }
 
 document.addEventListener('keydown', function(event) {
